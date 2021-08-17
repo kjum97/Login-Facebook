ee4a61d8b7453d9ed59a22a6ced46b1489c9f521
kjum97 <70919445+kjum97@users.noreply.github.com>
2021-08-17 15:51:47 -0600

    a

---
+++ js/validar.js
@@ -0,0 +1,43 @@
+const formulario = document.querySelector('.formulario')
+const botonSubmit = document.querySelector('#login');
+
+// Validar Formulario
+botonSubmit.addEventListener('click', function(evento) {
+	evento.preventDefault();
+	
+	// Inputs
+	const inputCorreo = document.querySelector('#correo');
+	const inputClave = document.querySelector('#clave');
+
+	// Añadir valor de los input en un objeto
+	const formInput = {
+		correo: inputCorreo.value,
+		clave: inputClave.value
+	}
+
+	// Comprobar que los inputs no están vacíos
+	if (formInput.correo === '' || formInput.clave === '') {
+		mostrarAlerta(false, "Todos los campos son obligatorio.");
+	} else {
+		mostrarAlerta(true, "Inicia sesion en la version mas reciente");
+	}
+
+
+
+	function mostrarAlerta(estado, mensaje) {
+		const crearDiv = document.createElement('DIV');
+		if (estado) {
+			crearDiv.classList.add('exito');
+			crearDiv.textContent = mensaje;
+		} else {
+			crearDiv.classList.add('error');
+			crearDiv.textContent = mensaje;
+		}
+		formulario.appendChild(crearDiv);
+
+		setTimeout(function () {
+				crearDiv.remove();
+			}, 5000);
+	}
+
+});
